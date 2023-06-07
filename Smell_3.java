5fd4408d742aeaecd2af8ce068bf9955984c218b
Kamel Alrashedy <kamel773@outlook.com>
2023-06-07 18:33:07 -0400

    Create Smell_3.java

---
+++ Smell_3.java
@@ -0,0 +1,3 @@
+public static byte[] encryptDES(final byte[] data,final byte[] key,final String transformation,final byte[] iv){
+  return symmetricTemplate(data,key,"DES",transformation,iv,true);
+}
